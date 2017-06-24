87f2f11f5f8bb1ec37bc12a4c2cb9cf922337bb4
joeylaguna <elagunaj@gmail.com>
2017-06-24 15:02:24 -0700

    Create questions component

---
+++ public/components/Questions.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Questions extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Inside questions</h1>
+      </div>
+    );
+  }
+}
+
+export default Questions;
