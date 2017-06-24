087382e46eb9f22b638249f1061de3786cb391a8
joeylaguna <elagunaj@gmail.com>
2017-06-24 15:02:49 -0700

    Include questions component

--- public/components/App.jsx
+++ public/components/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Questions from './Questions.jsx';
 
 class App extends React.Component {
   render() {
     return (
-      <h1>Hello</h1>
+      <Questions />
     )
   }
 }
