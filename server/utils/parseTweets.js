92d1933672704daed4ec3162b1a16c1f70dee070
Isaac Yoon <ysaacioon@yahoo.com>
2017-06-24 22:49:41 -0700

    adds sentimentAnalysis function

---
+++ server/utils/parseTweets.js
@@ -0,0 +1,19 @@
+
+module.exports = {
+	parseTweets: function(arr) {
+		console.log('arr', arr)
+		let tweets = []
+		for(var i = 0; i < arr.length; i ++)  {
+			var obj = arr[i];
+			for(var key in obj) {
+				if(key === 'text') {
+					tweets.push(obj[key])
+				}
+			}
+		}
+		console.log('tweets', tweets.length)
+		return tweets.join();
+	}
+
+}
+
