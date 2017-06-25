35b4bd4e04f96ff5c8b2a3948f764476d4cb82fd
Peter Yao <peteryao916@gmail.com>
2017-06-25 02:48:54 -0700

    aslkdfaksfdkl

--- public/components/AccountLink.jsx
+++ public/components/AccountLink.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import axios from 'axios';
 
+var RTM = require("satori-rtm-sdk");
+
 class AccountLink extends React.Component {
 	constructor(){
 		super();
@@ -12,12 +14,55 @@ class AccountLink extends React.Component {
       sexualOrientation: '',
       religiousBackground: '',
       politicalPreference: '',
-      twitterID: ''
+      twitterID: '',
+      otherName: '',
+      otherGender: '',
+      otherAge: '',
+      otherEthnicity: '',
+      otherSexualOrientation: '',
+      otherReligiousBackground: '',
+      otherPoliticalPreference: '',
+      otherTwitterID: ''
 		}
  	 	this.handleInputChange = this.handleInputChange.bind(this);
  	 	this.handleSubmit = this.handleSubmit.bind(this)
   }
 
+  findPartner() {
+    var endpoint = "wss://uv6r25xn.api.satori.com";
+    var appkey = "4EDedbecd2ab3Aedf6eBCBbC4bBA58AE";
+    var role = "default";
+    var roleSecretKey = "3EF46ECF5a1d1F365fdF37c0dA9e38d3";
+    var channel = "waitingroom";
+    var self = this;
+
+    var rtm = new RTM(endpoint, appkey);
+
+    var subscription = rtm.subscribe(channel, RTM.SubscriptionMode.SIMPLE);
+
+    /* set callback for PDU with specific action */
+    subscription.on('rtm/subscription/data', function (pdu) {
+      pdu.body.messages.forEach(function (msg) {
+        console.log('Got message:', msg);
+        if(self.state.name !== msg.name) {
+          self.setState({
+            otherName: msg.name,
+            otherGender: msg.gender,
+            otherAge: msg.age,
+            otherEthnicity: msg.ethnicity,
+            otherSexualOrientation: msg.sexualOrientation,
+            otherReligiousBackground: msg.religiousBackground,
+            otherPoliticalPreference: msg.politcalStuff,
+            otherTwitterID: msg.twitterID});
+          rtm.stop();
+      }
+      });
+      // close client after receving one message
+    });
+    if(rtm.isStopped()) {
+      rtm.start();
+    }
+  }
 
   handleInputChange(event) {
     const target = event.target;
@@ -50,6 +95,36 @@ class AccountLink extends React.Component {
         console.log(politcalStuff);
   		})
   	}
+    var endpoint = "wss://uv6r25xn.api.satori.com";
+    var appkey = "4EDedbecd2ab3Aedf6eBCBbC4bBA58AE";
+    var role = "default";
+    var roleSecretKey = "3EF46ECF5a1d1F365fdF37c0dA9e38d3";
+    var channel = "waitingroom";
+    var self = this;
+
+    var rtm = new RTM(endpoint, appkey);
+
+    var subscription = rtm.subscribe(channel, RTM.SubscriptionMode.SIMPLE);
+
+    /* publish a message after being subscribed to sync on subscription */
+    subscription.on('enter-subscribed', function () {
+      let newMessage = {
+        name: self.state.name,
+        gender: self.state.gender,
+        age: self.state.age,
+        ethnicity: self.state.ethnicity,
+        sexualOrientation: self.state.sexualOrientation,
+        religiousBackground: self.state.religiousBackground,
+        politicalPreference: self.state.politicalPreference,
+        twitterID: self.state.twitterID
+      };
+      rtm.publish(channel, newMessage, function (pdu) {
+        console.log("Publish ack:", pdu);
+      });
+      rtm.stop();
+
+    });
+    rtm.start();
     this.props.updateUserInfo(this.state);
 	}
 
